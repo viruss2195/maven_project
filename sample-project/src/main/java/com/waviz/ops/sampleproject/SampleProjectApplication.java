d8b3293e4800a1153f4fc6047cee3756a78e8af9
Aman <aman.waviz@gmail.com>
2018-08-31 18:15:32 +0530

    Update SampleProjectApplication.java

--- sample-project/src/main/java/com/waviz/ops/sampleproject/SampleProjectApplication.java
+++ sample-project/src/main/java/com/waviz/ops/sampleproject/SampleProjectApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleProjectApplication.class, args);
+		SpringApplication.run(SampleProjectApplication.class sdds, args);
 	}
 }
